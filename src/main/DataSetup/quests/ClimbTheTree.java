20fd50548cfd86f5d9922936875fb215fa4748da
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-09-15 21:32:33 -0300

    Update ClimbTheTree.java

--- src/main/DataSetup/quests/ClimbTheTree.java
+++ src/main/DataSetup/quests/ClimbTheTree.java
@@ -4,8 +4,10 @@ import main.DataSetup.entities.Player;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class ClimbTheTree extends Quest{
+    Scanner sc = new Scanner(System.in);
     public ClimbTheTree(Player player){
         super("climb the three", "camping", player);
     }
@@ -17,6 +19,22 @@ public class ClimbTheTree extends Quest{
         return false;
     }
     public void startQuest(){
-        System.out.print("");
+        System.out.print("\n" +
+                "Around the camp, there are many trees, but one in particular catches\nyour attention due to a minute detail: a nest at the top of it.");
+        String choice = sc.nextLine().toLowerCase();
+        if(choice.contains("c") || choice.contains("u")){
+            System.out.println("You're about 10 meters above the ground. Inside the nest, you see three eggs, and a fourth egg, much larger than the others, catches your eye.");
+
+            if(sc.nextLine().toLowerCase().contains("take")){
+                this.getPlayer().addItemToInventory("Mysterious Egg");
+            }else{
+                System.out.println("You leave the eggs alone");
+            }
+            System.out.println("Now, you need find a way to get down");
+            String finalAns = sc.nextLine().toLowerCase();
+            while(!finalAns.contains("d")){
+                System.out.println("There must be another way");
+            }
+        }
     }
 }
